ac37f4cd88158befd10572f6f8255323514dec2e
PingSuper <15498627+PingSuper@users.noreply.github.com>
2023-03-22 19:46:19 -0500

    Added Singleton Pattern

---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace SingletonPattern
+{
+	public class Singleton
+	{
+		// Private Constructor
+		private Singleton()
+		{
+		}
+
+		private static Singleton uniqueInstance;
+
+        public static Singleton getInstance()
+		{
+			if(uniqueInstance == null) {
+				uniqueInstance = new Singleton();
+			}
+			return uniqueInstance;
+		}
+
+		// other methods
+    }
+}
+
